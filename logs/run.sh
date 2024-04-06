e8d9799ac14a1c6e358218ab0cc40b5caa616402
Sergio Garcia <sergio@garciadelacruz.es>
2024-04-07 01:58:22 +0200

    Cleanup script

--- logs/run.sh
+++ logs/run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-set -e
+set -xe
 
-pushd .
+pushd . >/dev/null
 cd "$(dirname "$0")"
 
 # If no node modules are installed, install them
@@ -10,6 +10,9 @@ if [ ! -d "node_modules" ]; then
   npm install
 fi
 
-cat sockets.out.txt | ./node_modules/@pkmn/protocol/protocol-verifier
 
-popd
+cat sockets.out.txt |
+	# grep -v customgroups |
+	./node_modules/@pkmn/protocol/protocol-verifier
+
+popd >/dev/null
